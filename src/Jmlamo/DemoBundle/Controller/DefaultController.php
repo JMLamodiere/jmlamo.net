2f10859f9c3ce6ec5cfc0c03c61791985100ee52
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-11-09 11:32:57 +0100

    Switching routing from yml to annotation

--- src/Jmlamo/DemoBundle/Controller/DefaultController.php
+++ src/Jmlamo/DemoBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace Jmlamo\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
     public function indexAction($name)
     {
-        return $this->render('JmlamoDemoBundle:Default:index.html.twig', array('name' => $name));
+        return array(
+            'name' => $name,
+        );
     }
 }
