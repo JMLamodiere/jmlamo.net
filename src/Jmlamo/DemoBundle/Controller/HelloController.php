9309a57f8820024ac174cdb150fca1e77ca783cf
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-11-09 15:24:02 +0100

    Manual rendring of a twig template, and query string parameter exemple

--- src/Jmlamo/DemoBundle/Controller/HelloController.php
+++ src/Jmlamo/DemoBundle/Controller/HelloController.php
@@ -14,7 +14,7 @@ class HelloController extends Controller
      * url "/hello/world" matches worldAction AND indexAction, but the first one is called
      * @Route("/hello/world")
      */
-    public function worldAction(Request $request)
+    public function worldAction()
     {
         return new Response('<html><body>Hello world, sending new Response("html code")</body></html>');
     }
@@ -22,11 +22,27 @@ class HelloController extends Controller
     /**
      * @Route("/hello/wizard")
      */
-    public function wizardAction(Request $request)
+    public function wizardAction()
     {
         $response = $this->forward('JmlamoDemoBundle:Hello:index', array('firstname' => 'Harry', 'lastname' => 'Poster'));
         
         return $response;
+    }
+    
+    /**
+     * Ex : "/hello/manual-rendering?nameInQueryString=Sanchez"
+     * @Route("/hello/manual-rendering")
+     */
+    public function manualrenderingAction(Request $request)
+    {
+        $content = $this->renderView('JmlamoDemoBundle:Hello:index.html.twig', array(
+            'firstname' => 'Manual',
+            'lastname' => $request->get('nameInQueryString', 'Anonymous'),
+        ));
+        
+        return new Response($content);
+        
+        //or return $this->render(...
     }    
 
     /**
