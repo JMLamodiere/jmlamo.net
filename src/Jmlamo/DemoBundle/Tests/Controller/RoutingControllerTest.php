a104bc1d8ee25db04cdc26b3eec1ba51a8c513f9
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-11-10 16:18:20 +0100

    Testing a click on a link

--- src/Jmlamo/DemoBundle/Tests/Controller/RoutingControllerTest.php
+++ src/Jmlamo/DemoBundle/Tests/Controller/RoutingControllerTest.php
@@ -6,4 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoutingControllerTest extends WebTestCase
 {
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/demo/routing');
+        $this->assertTrue($crawler->filter('html:contains("Routing homepage")')->count() == 1);
+        
+        $link = $crawler->filter('a:contains("Page 2")')->eq(0)->link();
+        $crawler = $client->click($link);
+        
+        $this->assertTrue('/demo/routing/2' == $client->getRequest()->getRequestUri());
+    }
 }
