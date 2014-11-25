3540961c3a5df24dde3bb0a84a7e2abfc1e135ce
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-11-25 23:26:17 +0100

    createAction : creates a random product

--- src/Jmlamo/DemoBundle/Controller/DoctrineController.php
+++ src/Jmlamo/DemoBundle/Controller/DoctrineController.php
@@ -5,6 +5,7 @@ namespace Jmlamo\DemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Jmlamo\DemoBundle\Entity\Product;
 
 class DoctrineController extends Controller
 {
@@ -15,6 +16,27 @@ class DoctrineController extends Controller
     public function indexAction()
     {
         return array();
+    }
+    
+    /**
+     * @Route("/doctrine/create")
+     */
+    public function createAction()
+    {
+    
+        $product = new Product();
+        $product->setName('product ' . rand(1000, 9999));
+        $product->setPrice(rand(0, 999999) / 100);
+        $product->setDescription(str_repeat('lorem ipsum ', rand(10, 50)));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+        
+        $session = $this->get('session');
+        $session->getFlashBag()->add('notice', 'Product created : ' . $product->getName());
+        
+        return $this->redirect($this->generateUrl('jmlamo_demo_doctrine_index'));
     }
 
 }
