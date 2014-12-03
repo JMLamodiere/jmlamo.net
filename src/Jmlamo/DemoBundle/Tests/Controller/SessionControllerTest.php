fb7da095bce21797f9be4c04a5e809383ef32870
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-12-04 00:02:41 +0100

    add missing functionnal tests for session controller

--- src/Jmlamo/DemoBundle/Tests/Controller/SessionControllerTest.php
+++ src/Jmlamo/DemoBundle/Tests/Controller/SessionControllerTest.php
@@ -10,7 +10,20 @@ class SessionControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/session');
+        $crawler = $client->request('GET', '/demo/session');
+        $this->assertCount(1, $crawler->filter('.flash-message:contains("0 is an even number")'));
+        $this->assertCount(1, $crawler->filter('h1:contains("Counter : 0")'));
+        
+        $crawler = $client->reload();
+        $this->assertCount(0, $crawler->filter('.flash-message'));
+        $this->assertCount(1, $crawler->filter('h1:contains("Counter : 1")'));
+        
+        $crawler = $client->request('GET', '/demo/session');
+        $this->assertCount(1, $crawler->filter('h1:contains("Counter : 2")'));
+        
+        $client->restart();
+        $crawler = $client->request('GET', '/demo/session');
+        $this->assertCount(1, $crawler->filter('h1:contains("Counter : 0")'));
     }
 
 }
