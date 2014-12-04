72fbdd2044bdf0c469b612a3b1710e3f5f67dc51
Jean-Marie Lamodière <jmlamo@gmail.com>
2014-12-04 23:39:35 +0100

    Display a flashbag notice depending on author objet validity
    - Try to create an Author object with or without adding a name
    - Test its validity
    - Show result as a flashbag notice

--- src/Jmlamo/DemoBundle/Controller/ValidationController.php
+++ src/Jmlamo/DemoBundle/Controller/ValidationController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Jmlamo\DemoBundle\Entity\Author;
 
 class ValidationController extends Controller
 {
@@ -16,6 +19,40 @@ class ValidationController extends Controller
     public function indexAction()
     {
         return array();
+    }
+    
+    /**
+     * @Route("/validation/create/{name}")
+     */
+    public function createAction(Request $request, $name = null)
+    {
+        $validator = $this->get('validator');
+        $session = $request->getSession();
+        
+        $author = new Author();
+        
+        //for security reasons, we only allow "Jude"
+        if ('Jude' == $name) {
+            $author->setName($name);
+        }
+        
+        $errors = $validator->validate($author);
+        
+        if (count($errors) > 0) {
+            $session->getFlashBag()->add(
+                'notice',
+                // @see __toString()
+                (string) $errors
+            );
+            
+        } else {
+            $session->getFlashBag()->add(
+                'notice',
+                sprintf('%s is valid !', $author->getName())
+            );
+        }
+        
+        return $this->redirect($this->generateUrl('jmlamo_demo_validation_index'));
     }
 
 }
--- src/Jmlamo/DemoBundle/Tests/Controller/ValidationControllerTest.php
+++ src/Jmlamo/DemoBundle/Tests/Controller/ValidationControllerTest.php
@@ -13,6 +13,18 @@ class ValidationControllerTest extends WebTestCase
         //index
         $crawler = $client->request('GET', '/demo/validation');
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Validation homepage")')->count());
+        
+        //create (blank name -> fails)
+        $crawler = $client->request('GET', '/demo/validation/create');
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertCount(1, $crawler->filter('.flash-message:contains("should not be blank")'));
+        
+        //create (valid name)
+        $crawler = $client->request('GET', '/demo/validation/create/Jude');
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertCount(1, $crawler->filter('.flash-message:contains("is valid !")'));
     }
 
 }
